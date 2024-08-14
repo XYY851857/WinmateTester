fb081cbedea623cb32ad192f3e4c881a6fea7eff
XYY <chengyang@terchy.com.tw>
2024-08-14 13:19:13 +0800

    Update

--- folder/WR_subprocess.py
+++ folder/WR_subprocess.py
@@ -1,4 +1,3 @@
-import subprocess
 import os
 
 
@@ -17,14 +16,14 @@ def write(path, size_in_bytes):
                 file.write('0' * remaining_bytes)
                 total_written += remaining_bytes
 
-    print(f'已成功創建 {path}，大小為 {total_written} 字節。')
+    print(f'{path}, size:{total_written} bytes created.')
 
     # 驗證檔案大小
     actual_size = os.path.getsize(path)
     if actual_size == size_in_bytes:
-        print(f'檔案 {path} 實際大小為 {actual_size} 字節，大小驗證成功！')
+        print(f'{path}, size:{actual_size} bytes check size correct.')
     else:
-        print(f'錯誤：檔案 {path} 實際大小為 {actual_size} 字節，與預期的 {size_in_bytes} 字節不符！')
+        print(f'Error,  {path} actual size {actual_size} bytes, excepted {size_in_bytes} incorrect.')
         return False
 
     # 驗證檔案內容
@@ -32,15 +31,23 @@ def write(path, size_in_bytes):
         content = file.read()
         expected_content = '0' * size_in_bytes
         if content == expected_content:
-            print('檔案內容驗證成功！')
+            print(f'Disk:{path} Write & Read Success')
+            return True
         else:
-            print('錯誤：檔案內容不符！')
+            print('Error')
             return False
 
-    return True
 
 
 if __name__ == "__main__":
     # 指定檔案大小，例如 10 KB
-    file_size = int(input("input write size(MB):")) * 1024 * 1024 # 10 KB = 10 * 1024 字節
-    write(f'.\\test_file_{file_size/1024/1024:.0f}MB.txt', file_size)
+    file_size = int(input("input write size(MB):")) * 1024 * 1024
+    disk_list = ["E", "D", "E"]
+    for step in range(0, 2):
+        print(f'Testing Disk: {disk_list[step]}')
+        try:
+            file_path = f'{disk_list[step]}:\\test_file_{file_size/1024/1024:.0f}MB.txt'
+            state = write(file_path, file_size)
+            print(state)
+        except Exception as e:
+            print(e)
