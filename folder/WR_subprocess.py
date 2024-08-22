c2179b0dde0d554a0aa8ee85f3aeb2cd239a33ba
XYY <chengyang@terchy.com.tw>
2024-08-22 17:00:08 +0800

    修改定義存檔名稱，EXE REPO

--- folder/WR_subprocess.py
+++ folder/WR_subprocess.py
@@ -2,7 +2,7 @@ import os
 
 
 def write(path, size_in_bytes):
-    chunk_size = 1024  # 每次寫入的塊大小（以字節為單位）
+    chunk_size = 1024  # 每次寫入的大小（以字節為單位）
     data = '0' * chunk_size  # 填充檔案的資料
     total_written = 0
 
@@ -16,35 +16,28 @@ def write(path, size_in_bytes):
                 file.write('0' * remaining_bytes)
                 total_written += remaining_bytes
 
-    print(f'{path}, size:{total_written} bytes created.')
-
-    # 驗證檔案大小
     actual_size = os.path.getsize(path)
-    if actual_size == size_in_bytes:
-        print(f'{path}, size:{actual_size} bytes check size correct.')
-    else:
-        print(f'Error,  {path} actual size {actual_size} bytes, excepted {size_in_bytes} incorrect.')
-        return False
+    if actual_size != size_in_bytes:
+        with open('ERROR_report.txt', 'a') as errfile:
+            errfile.write(f'\nWR_subprocess: {path} write/read size incorrect.\n')
+        return "Failed"
 
-    # 驗證檔案內容
     with open(path, 'r') as file:
         content = file.read()
         expected_content = '0' * size_in_bytes
         if content == expected_content:
-            print(f'Disk:{path[:1]}:\\ Success')
-            return True
+            return 'PASS'
         else:
-            print('Error')
-            return False
+            with open('ERROR_report.txt', 'a') as errfile:
+                errfile.write(f'\nWR_subprocess: {path} write/read content incorrect.\n')
+            return "Failed"
 
 
 if __name__ == "__main__":
-    # 指定檔案大小，例如 10 KB
-    # file_size = int(input("input write size(MB):")) * 1024 * 1024
     file_size = 100 * 1024 * 1024  # 100 MB
 
     disk_list = ["D", "E"]
-    state = False
+    state = 'Failed'
     for step in range(0, 2):
         print(f'Testing Disk: {disk_list[step]}')
         file_path = f'{disk_list[step]}:\\test_file_{file_size / 1024 / 1024:.0f}MB.txt'
@@ -57,5 +50,5 @@ if __name__ == "__main__":
             if os.path.exists(file_path):
                 os.remove(file_path)
             print(e)
-        with open('WR_report.txt', 'a') as file:
+        with open('report.txt', 'a') as file:
             file.write(f'{disk_list[step]}:\\ Port {state}\n')
