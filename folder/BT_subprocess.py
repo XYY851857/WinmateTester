3acdcf00fd45b61a894586f97ba38e7c9470947d
XYY <chengyang@terchy.com.tw>
2024-08-23 17:17:30 +0800

    顯示文字修改，EXE REPO

--- folder/BT_subprocess.py
+++ folder/BT_subprocess.py
@@ -4,9 +4,9 @@ import subprocess
 def pair():
     try:
         result = subprocess.run(['powershell', '-Command', combined], capture_output=True, text=True, check=True)
-        print(f'Result: Bluetooth Connect Success')
+        print(f'藍牙: Bluetooth Connect Success')
         if result.stderr:
-            print(f'Error: \n {result.stderr}')
+            print(f'ERROR: \n {result.stderr}')
     except subprocess.CalledProcessError as e:  # ' System Error.  Code: 1244.使用者尚未被驗證，因此無法執行所要求的操作。'
         keyword = 'Code: '
         start_pos = e.stderr.find(keyword)
@@ -14,17 +14,17 @@ def pair():
             start_pos += len(keyword)
             result = e.stderr[start_pos:start_pos + 4]
             if result == '1244':  # 因無法操作OS故用error code判斷 1244:可連接但OS未點擊接收
-                print('Result: Bluetooth OK')
+                print('藍牙: Bluetooth OK')
                 return 'PASS'
             elif result[0:3] == "258":  # 連接逾時
-                print('Result: Connect Timeout')
+                print('藍牙: Connect Timeout')
                 return 'Failed'
             elif result[0:2] == '31':  # 已經連上，無法再次配對，判斷爲success
-                print('Result: Connected')
+                print('藍牙: Connected')
                 return 'PASS'
             else:
-                print(f"Result: Error:\n {e.stderr}")
-        print(f"Result: ERROR\n{e.stderr}")
+                print(f"藍牙: Error:\n {e.stderr}")
+        print(f"藍牙: ERROR\n{e.stderr}")
         with open('ERROR_report.txt', 'a') as errfile:
             errfile.write(f'BT_subprocess.py:  {e.stderr}\n')
         return f'ERROR\n{e.stderr}'
